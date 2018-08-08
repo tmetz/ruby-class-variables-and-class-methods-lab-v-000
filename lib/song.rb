e8d453be581005642912f3115518fc51d80c039e
Tammy Metz <tammy@tammymetz.com>
2018-08-08 23:50:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,4 +32,29 @@ class Song
       end
     artists_return
   end
+
+  def self.genre_count
+    genre_hash = {}
+    @@genres.each do |genre|
+      if !genre_hash.include?(genre)
+        genre_hash[genre] = 1
+      else
+        genre_hash[genre] += 1
+      end
+    end
+    genre_hash
+  end
+
+  def self.artist_count
+    artist_hash = {}
+    @@artists.each do |artist|
+      if !artist_hash.include?(artist)
+        artist_hash[artist] = 1
+      else
+        artist_hash[artist] += 1
+      end
+    end
+    artist_hash
+  end
+  
 end
